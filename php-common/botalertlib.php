b2188195236bc771910af333a28016776eb2f87f
manuel.bessler <manuel.bessler@6ff71aa2-1fbf-db3d-34bb-93e1ee95c5dc>
2010-06-21 19:59:41 +0000

    - added auto-wiring - cleaned up

--- php-common/botalertlib.php
+++ php-common/botalertlib.php
@@ -1,13 +1,26 @@
 <?php
 function botblock_VERDict($custid, $authtoken, $hpmxRequestId, $neutral_is_acceptable=1)
 {
-	$url = "http://" . $custid . ".botalert.com/VERD?custid=" . $custid . "&auth=" . $authtoken . "&reqid=" . $hpmxRequestId; 
-	$hpmxResult = file_get_contents( $url ); 
-	if( $hpmxResult == 1 ) // Human
-		return true; 
-	elseif( $hpmxResult == 0 && $neutral_is_acceptable == 1 ) 
-		return true; // Neutral, but acceptable 
-	return false;
+        $url = "http://" . $custid . ".botalert.com/VERD?custid=" . $custid . "&auth=" . $authtoken . "&reqid=" . $hpmxRequestId; 
+        $hpmxResult = file_get_contents( $url );
+        # http://pramana.com/resources/validation-results/
+        if( $hpmxResult == -1 || $hpmxResult == -3 ) // -1 = Bot, -3 = Backdoor
+                return false;
+        if( $hpmxResult == 0 && $neutral_is_acceptable != 1 ) // 0 = Neutral
+                return false; // Neutral, but not acceptable
+        return true; // 1 = Human, others are errors, but we let them through, so we can fail-open.
+}
+
+function botalert_AUTHenticateAUTOWIRE($custid, $authtoken, $frm, $submit, $fld)
+{
+        $use_ssl = false;
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on")
+                $use_ssl = true;
+        $url = "http://" . $custid . ".botalert.com/AUTH?custid=" . $custid . "&auth=" . $authtoken;
+        if( $use_ssl ) $url .= "&secure=1";
+        $url .= "&autowire=$frm|$submit|$fld";
+        $botalert_snippet = file_get_contents( $url );
+        return $botalert_snippet;
 }
 
 function botalert_AUTHenticate($custid, $authtoken)
@@ -17,18 +30,7 @@ function botalert_AUTHenticate($custid, $authtoken)
 		$use_ssl = true;
 	$url = "http://" . $custid . ".botalert.com/AUTH?custid=" . $custid . "&auth=" . $authtoken;
 	if( $use_ssl ) $url .= "&secure=1";
-	$botalert_snippet = 
-	'<div class="botalert">
-		<script type="text/javascript">
-			function triggerPramana(form, refId){
-				if(typeof SGAVY_HPMX != undefined && typeof SGAVY_HPMX.sendHPMXDataDirect != undefined) {
-					SGAVY_HPMX.sendHPMXDataDirect(form, refId);
-					setTimeout(\'document.getElementById("\' + frm.id + \'")\' +\'.submit()\',50);
-				}
-			}
-		</script>' .
-		file_get_contents( $url ) .'
-	</div>';
+	$botalert_snippet = file_get_contents( $url ) 
 	return $botalert_snippet;
 }
 ?>
