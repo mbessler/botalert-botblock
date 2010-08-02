fbbb8138bb781e191ce3ac59dcf09b5f41b54c4d
gautamvishal <gautamvishal@6ff71aa2-1fbf-db3d-34bb-93e1ee95c5dc>
2010-08-02 18:41:40 +0000

    Added a link to screenshot-3.png

--- wordpress/botalertbotblock/wp-botalert.php
+++ wordpress/botalertbotblock/wp-botalert.php
@@ -46,21 +46,21 @@ function delete_preferences() {
 register_deactivation_hook(__FILE__, 'delete_preferences');
 
 /* =============================================================================
-   BotAlert/BotBlock on Registration Form 
+   BotAlert/BotBlock on Registration Form
    ============================================================================= */
-   
+
 // Embed the BotAlert code into the registration form
 function embed_botalert($errors) {
     global $botalert_opt, $wpmu;
-   
+
     if ($botalert_opt['ba_registration']) {
-		
+
         if ($wpmu == 1) {
             $error = $errors->get_error_message('botalert'); ?>
             <label for="verification">Verification:</label>
             <?php echo($error ? '<p class="error">'.$error.'</p>' : '') ?>
             <?php echo($_GET['rerror']? '<p class="error">RERROR2:'.$_GET['rerror'].'</p>' : '') ?>
-            <?php 
+            <?php
         }
 	else {
             echo ($_GET['rerror']? '<p class="error">RERROR3' . $_GET['rerror'] . '</p>': '');
@@ -78,10 +78,10 @@ else
 // Check the BotBlock score
 function check_botalert_score() {
 	global $botalert_opt, $errors;
-	
+
    if (empty($_POST['hpmxRequestId']))
 		$errors['blank_captcha'] = $botalert_opt['botalert_error'];
-   
+
    else {
 	$botalert_result = botalert_VALI($_POST['user_login']);
 	if ( ! $botalert_result )
@@ -92,23 +92,23 @@ function check_botalert_score() {
 // Check the BotBlock score
 function check_botalert_score_new($errors) {
 	global $botalert_opt;
-	
+
    if (empty($_POST['hpmxRequestId']) ) {
 		$errors->add('blank_captcha', $botalert_opt['botalert_error']);
 		return $errors;
    }
-   
+
 	$botalert_result = botalert_VALI($_POST['user_login']);
 	if (! $botalert_result )
 		$errors->add('captcha_wrong', $botalert_opt['botalert_isbot']);
-   
+
    return $errors;
 }
 
 // Check Botalert/BotBlock on WordPress MU
 function check_botalert_score_wpmu($result) {
    global $_POST, $botalert_opt;
-   
+
    // must make a check here, otherwise the wp-admin/user-new.php script will keep trying to call
    // this function despite not having called do_action('signup_extra_fields'), so the recaptcha
    // field was never shown. this way it won't validate if it's called in the admin interface
@@ -129,14 +129,14 @@ function check_botalert_score_wpmu($result) {
             echo "<div class=\"error\">". $botalert_opt['botalert_isbot'] . "</div>";
       	}
    }
-   
+
    return $result;
 }
 
 if ($botalert_opt['ba_registration']) {
    if ($wpmu == 1)
 		add_filter('wpmu_validate_user_signup', 'check_botalert_score_wpmu');
-   
+
    else if ($wpmu == 0) {
 		// Hook the check_botalert_score function into WordPress
 		if (version_compare(get_bloginfo('version'), '2.5' ) >= 0)
@@ -154,7 +154,7 @@ if ($botalert_opt['ba_registration']) {
    ============================================================================= */
 
 $option_defaults = array (
-   'custid'	=> '', // the custid for BotAlert/BotBlock. Sign up at http://pramana.com 
+   'custid'	=> '', // the custid for BotAlert/BotBlock. Sign up at http://pramana.com
    'authtoken'	=> '', // the authtoken for BotAlert/BotBlock. Sign up at http://pramana.com
    'have_botblock' => '', // which product? BotAlert or BotBlock
    'treat_neutral_as_bad' => '0', // whether to treat a neutral score as Bot or Human
@@ -179,7 +179,7 @@ if ($wpmu != 1)
 function botalert_wp_hash_comment($id)
 {
 	global $botalert_opt;
-   
+
 	if (function_exists('wp_hash'))
 		return wp_hash(BOTALERT_WP_HASH_COMMENT . $id);
 	else
@@ -189,21 +189,21 @@ function botalert_wp_hash_comment($id)
 
 /**
  *  Embeds the BotAlert/BotBlock JS into the comment form.
- * 
- */	
+ *
+ */
 function botalert_comment_form() {
    global $user_ID, $botalert_opt;
 
 	// Did the user's behavior look suspiciously like a bot? If so, let them know
 	if ($_GET['rerror'] == 'botalert_isbot')
 		echo "<p style=\"font-size: 1.3em; padding-bottom: 8px;\">" . $botalert_opt['botalert_isbot'] . "</p>";
- 
+
 	if( !$botalert_opt['ba_comments'] ) // skip if disabled
 		return;
 
 	//modify the comment form for the BotAlert/BotBlock JS
 	$comment_string = <<<COMMENT_FORM
-		<div id="botalert-submit-btn-area"><br /></div> 
+		<div id="botalert-submit-btn-area"><br /></div>
 		<script type='text/javascript'>
 			var sub = document.getElementById('submit');
 			sub.parentNode.removeChild(sub);
@@ -215,7 +215,7 @@ function botalert_comment_form() {
 		</script>
 		<noscript>
 			<style type='text/css'>#submit {display:none;}</style>
-			<input name="submit" type="submit" id="submit-alt" tabindex="6" value="Submit Comment"/> 
+			<input name="submit" type="submit" id="submit-alt" tabindex="6" value="Submit Comment"/>
 		</noscript>
 COMMENT_FORM;
 
@@ -240,7 +240,7 @@ $botalert_saved_error = '';
 function botalert_wp_check_comment($comment_data) {
 	global $user_ID, $botalert_opt;
 	global $botalert_saved_error;
-	
+
 	// skip the filtering if the minimum capability is met
 	if (!$botalert_opt['ba_comments'])
 		return $comment_data;
@@ -268,21 +268,21 @@ function botalert_VALI($refid) {
 
 
 /*
- * If the BotBlock score indicated Bot from botalert_wp_check_comment, then redirect back to the comment form 
+ * If the BotBlock score indicated Bot from botalert_wp_check_comment, then redirect back to the comment form
  * @param string $location
  * @param OBJECT $comment
  * @return string $location
  */
 function botalert_wp_relative_redirect($location, $comment) {
 	global $botalert_saved_error;
-	if($botalert_saved_error != '') { 
+	if($botalert_saved_error != '') {
 		//replace the '#comment-' chars on the end of $location with '#commentform'.
 
 		$location = substr($location, 0,strrpos($location, '#')) .
 			((strrpos($location, "?") === false) ? "?" : "&") .
-			'rcommentid=' . $comment->comment_ID . 
+			'rcommentid=' . $comment->comment_ID .
 			'&rerror=' . $botalert_saved_error .
-			'&rchash=' . botalert_wp_hash_comment ($comment->comment_ID) . 
+			'&rchash=' . botalert_wp_hash_comment ($comment->comment_ID) .
 			'#commentform';
 	}
 	return $location;
@@ -290,7 +290,7 @@ function botalert_wp_relative_redirect($location, $comment) {
 
 /*
  * If the BotBlock score indicated Bot, from botalert_wp_check_comment, then insert their saved comment text
- * back in the comment form. 
+ * back in the comment form.
  * @param boolean $approved
  * @return boolean $approved
  */
@@ -379,15 +379,15 @@ function botalert_wp_options_subpanel() {
 /* =============================================================================
    BotAlert/BotBlock Admin Page and Functions
    ============================================================================= */
-   
+
 /*
- * Display an HTML <select> listing the capability options for disabling security 
- * for registered users. 
+ * Display an HTML <select> listing the capability options for disabling security
+ * for registered users.
  * @param string $select_name slug to use in <select> id and name
  * @param string $checked_value selected value for dropdown, slug form.
  * @return NULL
  */
- 
+
 function botalert_dropdown_capabilities($select_name, $checked_value="") {
 	// define choices: Display text => permission slug
 	$capability_choices = array (
@@ -406,7 +406,7 @@ function botalert_dropdown_capabilities($select_name, $checked_value="") {
 	endforeach;
 	echo "</select> \n";
  } // end botalert_dropdown_capabilities()
-   
+
 ?>
 
 <!-- ############################## BEGIN: ADMIN OPTIONS ################### -->
@@ -414,14 +414,14 @@ function botalert_dropdown_capabilities($select_name, $checked_value="") {
 	<h2>BotAlert/BotBlock Options</h2>
 	<h3>About BotAlert/BotBlock</h3>
 	<p>BotAlert and BotBlock are CAPTCHA replacement services. However, as opposed to all CAPTCHA solutions, BotAlert/BotBlock are invisible and do not require filling out an extra field. Learn more at <A HREF="http://pramana.com/botblock">http://pramana.com/botalert</A> and <A HREF="http://pramana.com/botblock">http://pramana.com/botblock</A>.</p>
-	
+
    <p><strong>NOTE</strong>: If you are using some form of Cache plugin you will probably need to flush/clear your cache for changes to take effect.</p>
-   
+
 	<form name="form1" method="post" action="<?php echo $_SERVER['REDIRECT_SCRIPT_URI'] . '?page=' . plugin_basename(__FILE__); ?>&updated=true">
 		<div class="submit">
 			<input type="submit" name="submit" value="<?php _e('Update Options') ?> &raquo;" />
 		</div>
-	
+
 	<!-- ****************** Operands ****************** -->
    <table class="form-table">
    <tr valign="top">
@@ -438,7 +438,7 @@ function botalert_dropdown_capabilities($select_name, $checked_value="") {
 			<p class="re-keys">
 				<!-- BotAlert custid -->
 				<label style="font-weight: bold;" for="botalert_opt_custid">CustID:</label>
-				<input name="botalert_opt_custid" id="botalert_opt_custid" size="40" value="<?php  echo $optionarray_def['custid']; ?>" />
+				<input name="botalert_opt_custid" id="botalert_opt_custid" size="40" value="<?php  echo $optionarray_def['custid']; ?>" /> <a href="http://www.pramana.com/images/screenshot-3.png" target="_blank">How to get this?</a>
 				<br />
 				<!-- BotAlert authtoken -->
 				<label style="font-weight: bold;" for="botalert_opt_authtoken">AuthToken:</label>
@@ -490,10 +490,10 @@ function botalert_dropdown_capabilities($select_name, $checked_value="") {
 						<option value="0" <?php if($optionarray_def['have_botblock'] == '0'){echo 'selected="selected"';} ?>>BotAlert</option>
 					</select>
 			    	</div>
-<P>				
+<P>
 		    	<!-- Whether or not to be XHTML 1.0 Strict compliant -->
 				<input type="checkbox" name="treat_neutral_as_bad" id="treat_neutral_as_bad" value="1" <?php if($optionarray_def['treat_neutral_as_bad'] == true){echo 'checked="checked"';} ?> /> <label for="treat_neutral_as_bad">Treat neutral scores as Bot?<strong><BR>Note</strong>: If checked, then any neutral score will be treated as a Bot. (Makes the scoring more strict)</label>
-</P><P>				
+</P><P>
 				<!-- select noscript behavior -->
 				<div style="vertical-align: middle !important;">
 					<label for="ba_noscript_action">How to handle clients without Javascript Support&sup1;:</label>
@@ -508,7 +508,7 @@ function botalert_dropdown_capabilities($select_name, $checked_value="") {
 			</td>
 		</tr>
 	</table>
-	
+
 	<div class="submit">
 		<input type="submit" name="submit" value="<?php _e('Update Options') ?> &raquo;" />
 	</div>
@@ -554,23 +554,23 @@ if( isset($_POST['submit']) && (!isset($_POST['botalert_opt_authtoken']) || strl
 if( count($ba_warnings) > 0 ) {
    function botalert_settings_warning() {
 		global $ba_warnings;
-		
+
 		$path = plugin_basename(__FILE__);
 		$top = 0;
 		if ($wp_version <= 2.5)
 		$top = 12.7;
 		else
 		$top = 7;
-                if( in_array('curl', $ba_warnings)) 
+                if( in_array('curl', $ba_warnings))
 		    echo "<div id='botalert-warning' class='error fade-ff0000'><p><strong>BotAlert/BotBlock is not active</strong> You must install <A HREF='http://php.net/manual/en/book.curl.php'>php-cURL</A> for it to work</p></div>";
-                if( in_array('custid', $ba_warnings)) 
+                if( in_array('custid', $ba_warnings))
 		    echo "<div id='botalert-warning' class='updated fade-ff0000'><p><strong>BotAlert/BotBlock is not active</strong> Your BotAlert/BotBlock CustID is emtpy or invalid. Check the <a href='options-general.php?page=" . $path . "'>Plugin Settings</a></p></div>";
-                if( in_array('authtoken', $ba_warnings)) 
+                if( in_array('authtoken', $ba_warnings))
 		    echo "<div id='botalert-warning' class='updated fade-ff0000'><p><strong>BotAlert/BotBlock is not active</strong> Your BotAlert/BotBlock AuthToken is emtpy or invalid. Check the <a href='options-general.php?page=" . $path . "'>Plugin Settings</a></p></div>";
 
-                if( in_array('custid2', $ba_warnings)) 
+                if( in_array('custid2', $ba_warnings))
 		    echo "<div id='botalert-warning' class='error fade-ff0000'><p><strong>BotAlert/BotBlock is not active</strong> Your BotAlert/BotBlock CustID is emtpy or invalid.</p></div>";
-                if( in_array('authtoken2', $ba_warnings)) 
+                if( in_array('authtoken2', $ba_warnings))
 		    echo "<div id='botalert-warning' class='error fade-ff0000'><p><strong>BotAlert/BotBlock is not active</strong> Your BotAlert/BotBlock AuthToken is emtpy or invalid.</p></div>";
 		echo "
 		<style type='text/css'>
@@ -578,10 +578,10 @@ if( count($ba_warnings) > 0 ) {
 		</style>
 		";
    }
-   
+
    if (($wpmu == 1 && is_site_admin()) || $wpmu != 1)
 		add_action('admin_footer', 'botalert_settings_warning');
-   return; 
+   return;
 }
 
 
